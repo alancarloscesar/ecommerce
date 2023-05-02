6a84ba3810b52e3353ec257a15ccf3b1f00e1f04
alancesar <alancarloscesar@gmail.com>
2023-05-02 11:31:29 -0300

    Login Creating login and authentication

--- backend/src/services/authUserService.ts
+++ backend/src/services/authUserService.ts
@@ -1,5 +1,7 @@
 
 import { compare } from "bcryptjs";
+import prismaClient from "../prisma";
+import { throws } from "assert";
 
 interface authData {
     email: string,
@@ -10,5 +12,34 @@ interface authData {
 export class AuthUserService {
     async execute({ email, cpf, password }: authData) {
 
+        const user = await prismaClient.user.findFirst({
+            where: {
+                OR: [
+                    {
+                        email: {
+                            equals: email
+                        }
+                    },
+                    {
+                        cpf: {
+                            equals: cpf
+                        }
+                    }
+                ]
+            }
+        })
+
+        if (!user) {
+            throw new Error("Email/Cpf/Senha incorretos!")
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch) {
+            throw new Error("Email/Cpf/Senha incorretos!")
+        }
+        const yes = "Login efetuado com sucesso"
+
+        return yes
     }
 }
