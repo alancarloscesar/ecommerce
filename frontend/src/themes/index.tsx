5bdb7b46a15ac0e2ae53c1f0e39f9ad7fe259d6b
alancesar <alancarloscesar@gmail.com>
2023-05-15 21:49:10 -0300

    theme config frontend by materialUi

---
+++ frontend/src/themes/index.tsx
@@ -0,0 +1,34 @@
+import { createTheme } from '@mui/material/styles';
+
+
+export const themeApp = createTheme({
+    
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#331A5C',
+            contrastText: '#fff',
+
+        },
+        secondary: {
+            main: '#fbb03b',
+            contrastText: '#fff',
+        },
+        background: {
+            default: '#f1f1f1',
+        },
+        text: {
+            primary: 'rgba(109,109,109,0.87)',
+        },
+
+    },
+    // typography: {
+    //     button: {
+    //         fontSize: '5rem',
+    //         cursor: 'progress'
+    //     },
+
+    // },
+ 
+})
+
